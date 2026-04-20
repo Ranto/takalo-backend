5c5a5dda0d0a002b766ac120211d838d4fdfe3fa
Ranto Ambinintsoa ANDRIANJAFY <andrianjafy@gmail.com>
2026-04-20 11:09:38 +0300

    Initial project

---
+++ src/main/java/ara/project/takalo/TakaloApplication.java
@@ -0,0 +1,13 @@
+package ara.project.takalo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakaloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakaloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ara/project/takalo/TakaloApplicationTests.java
@@ -0,0 +1,13 @@
+package ara.project.takalo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakaloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
