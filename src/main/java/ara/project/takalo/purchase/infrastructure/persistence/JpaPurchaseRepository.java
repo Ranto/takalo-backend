e9d62752cb9b71e63f7314c3b970bb8e8ce67d95
Ranto Ambinintsoa ANDRIANJAFY <andrianjafy@gmail.com>
2026-04-26 08:28:01 +0300

    fix: load items in purchase to have correct value for PurchaseLightResponse

--- src/main/java/ara/project/takalo/purchase/infrastructure/persistence/JpaPurchaseRepository.java
+++ src/main/java/ara/project/takalo/purchase/infrastructure/persistence/JpaPurchaseRepository.java
@@ -3,6 +3,7 @@ package ara.project.takalo.purchase.infrastructure.persistence;
 import ara.project.takalo.purchase.infrastructure.persistence.entities.PurchaseEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -25,6 +26,7 @@ public interface JpaPurchaseRepository extends JpaRepository<PurchaseEntity, UUI
           AND (:endDate IS NULL OR p.purchaseDate <= :endDate)
         """
     )
+    @EntityGraph(attributePaths = "items")
     Page<PurchaseEntity> findByDateRange(
             @Param("startDate") Instant startDate,
             @Param("endDate") Instant endDate,
